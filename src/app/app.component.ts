72c82d6cd2cfa45b418b1e3b75929d45da3bbeb4
lein <alain.vg.96@gmail.com>
2025-02-17 07:56:46 -0500

    intro forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'usted';
--- src/app/components/housing.service.ts
+++ src/app/components/housing.service.ts
@@ -10,9 +10,9 @@ export class HousingService {
     'https://i.pinimg.com/474x/24/77/9e/24779eb8b0ffffffa0766694c777d4ae.jpg';
   housinglocation: HousingLocation[] = [
     {
-      id: 999,
-      name: 'Test Home',
-      city: 'Test City',
+      id: 1,
+      name: 'Test Home 1',
+      city: 'Test City 1',
       state: 'ST',
       photo: `${this.basePhotoUrl}`,
       availableUnits: 99,
@@ -20,9 +20,9 @@ export class HousingService {
       laundry: false,
     },
     {
-      id: 999,
-      name: 'Test Home',
-      city: 'Test City',
+      id: 2,
+      name: 'Test Home 2',
+      city: 'Test City 2',
       state: 'ST',
       photo: `${this.basePhotoUrl}`,
       availableUnits: 99,
@@ -30,9 +30,9 @@ export class HousingService {
       laundry: false,
     },
     {
-      id: 999,
-      name: 'Test Home',
-      city: 'Test City',
+      id: 3,
+      name: 'Test Home 3',
+      city: 'Test City 3',
       state: 'ST',
       photo: `${this.basePhotoUrl}`,
       availableUnits: 99,
@@ -47,5 +47,11 @@ export class HousingService {
 
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housinglocation.find((housing) => housing.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string): void {
+    console.log(
+      `First Name: ${firstName} - Last Name: ${lastName} - Email: ${email}`,
+    );
   }
 }
